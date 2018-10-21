f4e65cd80a1595955e870cd0549c9e80c7791af2
voidiker66 <asi327@utexas.edu>
2018-10-20 21:29:57 -0700

    login users

--- app.py
+++ app.py
@@ -20,11 +20,127 @@ import datetime
 import flight
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
 app.config.update(dict(
 	SECRET_KEY="powerful secretkey",
 	WTF_CSRF_SECRET_KEY="a csrf secret key"
 ))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/dream.db'
+
+e = create_engine('sqlite:///database/dream.db')
+
+login_manager = LoginManager()
+
+
+@login_manager.user_loader
+def get_user(ident):
+  return User.query.get(int(ident))
+
+class User(db.Model, UserMixin):
+	__tablename__ = 'User'
+	id = db.Column(db.Integer, primary_key = True)
+	username = db.Column(db.String(32))
+	firstname = db.Column(db.String(32))
+	lastname = db.Column(db.String(32))
+	email = db.Column(db.String(32))
+	password = db.Column(db.String(32))
+
+	def __init__(self, username, firstname, lastname, email, password):
+		self.username = username
+		self.set_password(password)
+		self.email = email
+		self.firstname = firstname
+		self.lastname = lastname
+
+	def set_password(self, password):
+		self.password = generate_password_hash(password)
+
+	def check_password(self, password):
+		return check_password_hash(self.password, password)
+		#return password == self.password
+
+class LoginForm(Form):
+	username = TextField('Username', [validators.Required()])
+	password = PasswordField('Password', [validators.Required()])
+	submit = SubmitField('Log In')
+
+	def __init__(self, *args, **kwargs):
+		Form.__init__(self, *args, **kwargs)
+		self.user = None
+
+	def validate(self):
+		user = User.query.filter_by(
+			username=self.username.data).first()
+		if user is None:
+			self.username.errors.append('Unknown username')
+			return False
+
+		if not user.check_password(self.password.data):
+			self.password.errors.append('Invalid password')
+			return False
+
+		self.user = user
+		login_user(user)
+		return True
+
+class RegisterForm(Form):
+	username = TextField('Username', validators=[validators.Required()])
+	email = TextField('E-Mail', validators=[validators.Required(), validators.Email()])
+	password = PasswordField('Password', [
+		validators.Required(),
+		validators.EqualTo('confirm', message='Passwords must match')
+	])
+	confirm = PasswordField('Repeat Password')
+	firstname = TextField('First Name', validators=[validators.Required(), validators.Length(min=8, max=32, message="Password must be between 8 and 32 characters long")])
+	lastname = TextField('Last Name', validators=[validators.Required()])
+	submit = SubmitField('Submit')
+
+	def __init__(self, *args, **kwargs):
+		Form.__init__(self, *args, **kwargs)
+
+	def validate(self):
+		if self.username.data and self.password.data and self.confirm.data:
+			if User.query.filter_by(username=self.username.data).first():
+				flash('An account with that username already exists.', category='red')
+				return False
+			if User.query.filter_by(email=self.email.data).first():
+				flash('An account with that email already exists.', category='red')
+				return False
+			return True
+		return False
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		if form.validate():
+			# flash("You're now logged in!", category='green')
+			return redirect('/')
+		else:
+			flash("No user with that email/password combo", category='red')
+	return render_template('login.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+	form = RegisterForm()
+	if form.validate_on_submit():
+		if form.validate():
+			user = User(form.username.data, form.firstname.data, form.lastname.data, form.email.data, form.password.data)
+			db.session.add(user)
+			db.session.commit()
+			flash("You're now registered!", category='green')
+			return redirect('/login')
+		else:
+			flash("Error: Check your inputs", category='red')
+	return render_template('register.html', form=form)
+
+@app.route("/logout")
+@login_required
+def logout():
+	logout_user()
+	return redirect('/')
 
 def get_currency():
 	return ['USD', 'EUR', 'CNY']
@@ -84,6 +200,21 @@ def dash():
 		events = get_events(location, start, end).json()
 		dashdata.append(events)
 	return render_template('dashboard.html', data=dashdata)
+
+
+@login_manager.unauthorized_handler
+def unauthorized_callback():
+	return redirect('/login')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+login_manager.init_app(app)
+
+manager = APIManager(app, flask_sqlalchemy_db=db)
+manager.create_api(User, methods=['GET'],results_per_page=10)
 
 
 if __name__ == "__main__":
